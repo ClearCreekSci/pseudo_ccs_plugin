33b68556d9bc79af93505d025f656498f9c60e27
Winslow <winslow@clearcreeksci.com>
2026-02-09 03:35:14 -0500

    Add copyright info

--- pseudo.py
+++ pseudo.py
@@ -1,7 +1,23 @@
-# Copyright (c) 2025 Clear Creek Scientific
-#
-#         
+"""
+    pseudo.py
+    CCS datalogger plugin to simulate different sensors based on configuration 
 
+    Copyright (C) 2026 Clear Creek Scientific
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details. 
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
 import os
 import ccs_base
 import random
